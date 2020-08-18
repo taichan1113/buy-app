724e42c9f4605e556699656c927f1b73bbb32c19
taichan <takecchi.1113@gmail.com>
2020-08-19 01:36:45 +0900

    Updates

---
+++ precache-manifest.3b08834e9de3c4a422aeb20a5f6b379d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7727891022528aadf15c4ed8784c505f",
+    "url": "/buy-app/index.html"
+  },
+  {
+    "revision": "cc43e97e858337a9e0db",
+    "url": "/buy-app/static/css/main.83f91e4f.chunk.css"
+  },
+  {
+    "revision": "2acf403c6d5767bb972a",
+    "url": "/buy-app/static/js/2.f06fff38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/buy-app/static/js/2.f06fff38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc43e97e858337a9e0db",
+    "url": "/buy-app/static/js/main.da212c3c.chunk.js"
+  },
+  {
+    "revision": "387859b43267fc67abd9",
+    "url": "/buy-app/static/js/runtime-main.5fe038a5.js"
+  }
+]);
